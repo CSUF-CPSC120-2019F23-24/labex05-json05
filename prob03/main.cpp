5d174a6f4c483bacb19dd8d2a63b9391916a940c
json05 <json05@cs104u-1g.acad.fullerton.edu>
2019-09-21 13:53:36 -0700

    frame

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,17 @@
 // Random Rectangle
+
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+  std::string pound;
+  pound.assign("#");
+
+  unsigned int x = 40;
+  srand(x);
+
+
+
+  return 0;
+}
