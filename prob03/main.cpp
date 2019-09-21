c8d2bb04ac7d1dbbad28987bf17b4b27c3080eed
json05 <json05@cs104u-1g.acad.fullerton.edu>
2019-09-21 14:27:46 -0700

    created random rectangle generator

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,17 +1,28 @@
 // Random Rectangle
 
 #include <iostream>
+#include <string>
 #include <cstdlib>
+#include <ctime>
 
 int main()
 {
+  // Creates random seed each time program is run.
+  unsigned seed = time(0);
+  srand(seed);
+
+  // Creates random length each time
+  const int length = 40+1;
+  int width = rand() % length;
+
+  // Assigns # to width
   std::string pound;
-  pound.assign("#");
+  pound.assign(width, '#');
 
-  unsigned int x = 40;
-  srand(x);
-
-
+  // Displays rectangle.
+  std::cout << pound << '\n';
+  std::cout << pound << '\n';
+  std::cout << pound << '\n';
 
   return 0;
 }
